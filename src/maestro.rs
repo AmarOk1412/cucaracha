1274d961c708969c53be961ef2b1f21613c0a5f6
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-09-30 22:53:00 -0400

    maestro: start to write lib

--- src/maestro.rs
+++ src/maestro.rs
@@ -1,40 +1,52 @@
 
-use std::io;
-use std::time::Duration;
-
 use std::io::prelude::*;
 use serial::prelude::*;
 
-pub struct Maestro {
-    port: Box<dyn SerialPort>
+pub struct Settings {
+    pub port: String,
+    pub device: u8
 }
 
-const SETTINGS: serial::PortSettings = serial::PortSettings {
-    baud_rate:    serial::Baud9600,
-    char_size:    serial::Bits8,
-    parity:       serial::ParityNone,
-    stop_bits:    serial::Stop1,
-    flow_control: serial::FlowNone,
-};
+pub struct Maestro {
+    port: Box<dyn SerialPort>,
+    device: u8
+}
 
 impl Maestro {
     pub fn new() -> Maestro {
-        Maestro::new_with_port(String::from("/dev/ttyACM0"))
+        Maestro::new_with_settings(Settings {
+            port: String::from("/dev/ttyACM0"),
+            device: 0x0c
+        })
     }
 
-    pub fn new_with_port(port: String) -> Maestro {
-        let mut port = serial::open(&port).unwrap();
-        port.configure(&SETTINGS).ok().expect("Cannot configure Maestro");
+    pub fn new_with_settings(settings: Settings) -> Maestro {
+        let mut port = serial::open(&settings.port).unwrap();
+        let port_settings: serial::PortSettings = serial::PortSettings {
+            baud_rate:     serial::Baud9600,
+            char_size:     serial::Bits8,
+            parity:        serial::ParityNone,
+            stop_bits:     serial::Stop1,
+            flow_control:  serial::FlowNone,
+        };
+        port.configure(&port_settings).ok().expect("Cannot configure Maestro");
         Maestro {
-            port: Box::new(port)
+            port: Box::new(port),
+            device: settings.device
         }
     }
 
-    pub fn set_target(&mut self, channel: u8, target: u16) {
+    fn send(&mut self, data: &mut Vec<u8>) -> bool {
+        let mut buf = vec![0xaa, self.device];
+        buf.append(data);
+        debug!("send {:?}", data);
+        self.port.write(&data[..]).is_ok()
+    }
+
+    pub fn set_target(&mut self, channel: u8, target: u16) -> bool {
         let lsb = (target & 0x7f) as u8;
         let msb = (target >> 7 & 0x7f) as u8;
-        let data = vec![0xaa, 0x0c, 0x04, channel, lsb, msb];
-        println!("{:?}", data);
-        self.port.write(&data[..]); // TODO check ok
+        let mut data = vec![0x04, channel, lsb, msb];
+        self.send(&mut data)
     }
 }
